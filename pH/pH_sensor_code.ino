23bdf1fb577811d87c37406766dfbeb02424786e
seaghanabreo <78320073+seaghanabreo@users.noreply.github.com>
2021-02-23 00:01:42 +0400

    Add files via upload

---
+++ pH/pH_sensor_code.ino
@@ -0,0 +1,43 @@
+ 
+float calibration_value = 21.34 - 0.7;
+int phval = 0; 
+unsigned long int avgval; 
+int buffer_arr[10],temp;
+ 
+float ph_act;
+
+ 
+void setup() 
+{
+ Serial.begin(9600);
+ pinMode(A0,INPUT);
+}
+void loop() {
+ for(int i=0;i<10;i++) 
+ { 
+ buffer_arr[i]=analogRead(A0);
+ delay(30);
+ }
+ for(int i=0;i<9;i++)
+ {
+ for(int j=i+1;j<10;j++)
+ {
+ if(buffer_arr[i]>buffer_arr[j])
+ {
+ temp=buffer_arr[i];
+ buffer_arr[i]=buffer_arr[j];
+ buffer_arr[j]=temp;
+ }
+ }
+ }
+ avgval=0;
+ for(int i=2;i<8;i++)
+ avgval+=buffer_arr[i];
+ float volt=(float)avgval*5.0/1024/6; 
+  ph_act = -5.70 * volt + calibration_value;
+ 
+ Serial.println(",");
+ Serial.print(ph_act);
+ delay(1000);
+}
+ 
