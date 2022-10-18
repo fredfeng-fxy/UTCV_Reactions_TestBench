024902dce5e152cb05c024fb18afd9543f221226
mshtcchan <46391251+mshtcchan@users.noreply.github.com>
2022-10-18 14:53:54 -0400

    Descriptive Edits to Colour Sensor Code
    Added description of what the testing code does, and made  folder for it

--- Colour_Sensor/Colour_Sensor.ino
+++ Colour_Sensor/Colour_Sensor.ino
@@ -18,24 +18,27 @@ int prev_sum = 0;
 long prev_avg = 0;
 long first_diff = 0;
 
-/* Connect SCL to analog 5
-Connect SDA to analog 4
-Connect VDD to 3.3V DC
-Connect GROUND to common ground */
+/* Connect SCL (blue) to analog 5
+Connect SDA (green)to analog 4
+Connect VDD *purpl) to 3.3V DC
+Connect GROUND (grey) to common ground */
 /* Initialise with default values (int time = 2.4ms, gain = 1x) */
 Adafruit_TCS34725 tcs = Adafruit_TCS34725(TCS34725_INTEGRATIONTIME_120MS, TCS34725_GAIN_1X);
 /* Initialise with specific int time and gain values */
 
-// Car Motor
-const int car_motor_speed = 5;
-const int car_motor_fwd = 8; 
-const int car_motor_rev = 9;
+// Car Motor Pins //Comments are the old pins the arduino is connected to
+const int car_motor_speed = 5; //5
+const int car_motor_fwd = 8; //8
+const int car_motor_rev = 9; //9
 
-//Stirrer
+//Stirrer Pins
 
-const int stirrer_speed = 3;
-const int stirrer_fwd = 6;
-const int stirrer_rev = 7;
+const int stirrer_speed = 6; //3
+const int stirrer_fwd = 7; //6
+const int stirrer_rev = 8; //7
+
+// Stirrer speed 
+const int stir_rotating_speed = 80;
 
 
 // button
@@ -53,8 +56,9 @@ void setup(void) {
 // Now we're ready to get readings!
     //setup queue
     for (int i = 0; i < (MAX_IDX + 1); i++){
-      cur_tf[i] = 0;
-      prev_tf[i] = 0;
+      cur_tf[i] = 0; // used to find running average
+      prev_tf[i] = 0; // 
+      // t0-11, t0-10,...,t0-1 , t0
     }
     // Car Motor Setup
     pinMode(car_motor_speed, OUTPUT);
@@ -62,7 +66,6 @@ void setup(void) {
     pinMode(car_motor_rev, OUTPUT);
 
     // Stirrer Setup
-
     pinMode(stirrer_speed, OUTPUT);
     pinMode(stirrer_fwd, OUTPUT);
     pinMode(stirrer_rev, OUTPUT);
@@ -75,7 +78,7 @@ void setup(void) {
     // stirrer initialization
     digitalWrite(stirrer_fwd, LOW);
     digitalWrite(stirrer_rev, LOW);
-    analogWrite(stirrer_speed, 255);
+    analogWrite(stirrer_speed, stir_rotating_speed);
 
 }
 
@@ -115,7 +118,7 @@ if(Serial.available()){
         // stirrer initialization
         digitalWrite(stirrer_fwd, LOW);
         digitalWrite(stirrer_rev, LOW);
-        analogWrite(stirrer_speed, 255);
+        analogWrite(stirrer_speed, stir_rotating_speed);
         Started = 0;
     }
 }
@@ -125,7 +128,7 @@ if(Started){
     tcs.getRawData(&r, &g, &b, &c);
     currentTime = millis();
     timeDiff = currentTime - startTime;
-    if(timeDiff > 5000){
+    if(timeDiff > 5000){ //turn off stirrer after 5 seconds (we could put the 5000 in a variable)
         digitalWrite(stirrer_fwd, LOW);
         digitalWrite(stirrer_rev, LOW);  
     }
@@ -136,18 +139,18 @@ if(Started){
       cur_sum += cur_tf[i];
     }
     
-    cur_avg = cur_sum / (MAX_IDX + 1);
+    cur_avg = cur_sum / (MAX_IDX + 1); // takes average of the values in the cur_tf array
 
     prev_sum = 0;
     for (int i = 0; i < (MAX_IDX + 1); i++){
-      prev_sum += prev_tf[i];
+      prev_sum += prev_tf[i]; // 
     }
 
-    prev_avg = prev_sum / (MAX_IDX + 1);
+    prev_avg = prev_sum / (MAX_IDX + 1); //take avergae of values in the prev_tf array
     
     first_diff = cur_avg - prev_avg;
     if(initValue){
-      if(timeDiff > 10000){
+      if(timeDiff > 10000){ // won't check colour until 10 seconds into run
         initValue=0;
         }
     }else{
@@ -164,7 +167,7 @@ if(Started){
         Started = 0;
       }
     }
-    prev_tf[read_idx] = c;
+    prev_tf[read_idx] = c; //put the value we just read into the previous timeframe array
     read_idx++;
     if (read_idx > MAX_IDX){
       read_idx = 0;
